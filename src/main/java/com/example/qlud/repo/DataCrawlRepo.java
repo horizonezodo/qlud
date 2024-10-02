c96a984ff938fca0e207a80d399c45b94d42b282
horizonezodo <thiensutoloi3@gmail.com>
2024-10-02 14:48:46 +0700

    delete all

--- src/main/java/com/example/qlud/repo/DataCrawlRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.qlud.repo;
-
-import com.example.qlud.model.DataCrawl;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DataCrawlRepo extends MongoRepository<DataCrawl, String> {
-}
--- src/main/java/com/example/qlud/repo/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.qlud.repo;
-
-import com.example.qlud.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepo extends MongoRepository<Product, String> {
-}
